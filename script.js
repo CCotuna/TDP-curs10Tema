9828a30489cac6d72523c33295104e86e077bfc2
CCotuna <calin.cotuna@emanuel.ro>
2023-06-02 21:13:38 +0300

    Drew a circle

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ function setup() {
     createCanvas(900, 900);
   }
   function draw() {
-    translate(width / 2, height );
-    fractalStairs();
+    background("gray")
+    fractalStairs(100);
   }
 
 
-  function fractalStairs(){
-    let numberOfSquares = 100;
+  function fractalStairs(squares){
+    let numberOfSquares = squares;
     let step = 5; // the size
     let angle = TWO_PI / numberOfSquares;
     let size = step;
     let x = 450;
     let y = 450;
-    
+
+    for(let i = 0; i < numberOfSquares; i++){
+      rectMode(CENTER);
+      rect(x, y, size, size);
+      translate(x,y);
+      rotate(angle);
+      translate(-x, -y);
+      x += step * cos(angle);
+      y += step * sin(angle);
+    }
   }
