304f0e47d954a4f8930cc97493be10d6987b647f
CCotuna <calin.cotuna@emanuel.ro>
2023-06-02 21:17:43 +0300

    Calculated the x and y regarding canvas' width and height + working on removing the squares' color

--- script.js
+++ script.js
@@ -12,10 +12,11 @@ function setup() {
     let step = 5; // the size
     let angle = TWO_PI / numberOfSquares;
     let size = step;
-    let x = 450;
-    let y = 450;
+    let x = width/2;
+    let y = height/2;
 
     for(let i = 0; i < numberOfSquares; i++){
+      // nofill();
       rectMode(CENTER);
       rect(x, y, size, size);
       translate(x,y);
