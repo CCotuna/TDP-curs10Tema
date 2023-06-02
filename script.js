6e0ac75df3827f9f67e40e5757b5f4927e57bae0
CCotuna <calin.cotuna@emanuel.ro>
2023-06-02 21:09:16 +0300

    Declared some variables I'll need - Size-Angle-InitialCoordinates-NumOfSquares

--- script.js
+++ script.js
@@ -1,37 +1,18 @@
 function setup() {
-    createCanvas(900, 600);
+    createCanvas(900, 900);
   }
   function draw() {
-    angle = PI / 4;
     translate(width / 2, height );
-    treeBranch(100, 1);
+    fractalStairs();
   }
-  
-  function treeBranch(length, thickness) {
-    if (length > 60) {
-      stroke("green");
-      fill("red");
-    } else if (length > 30) {
-      stroke("gray");
-      fill("white");
-    } else {
-      stroke("black");
-      fill("green");
-    }
-    strokeWeight(thickness * 0.5);
-    // line(0, 0, 0, -length);
-    rect(0, length, length);
-    translate(0, -length);
-    if (length > 10) {
-      //partea dreapta
-      push();
-      rotate(angle);
-      treeBranch(length * 0.75, thickness);
-      pop();
-      //partea stanga
-      push();
-      rotate(-angle);
-      treeBranch(length * 0.75, thickness);
-      pop();
-    }
+
+
+  function fractalStairs(){
+    let numberOfSquares = 100;
+    let step = 5; // the size
+    let angle = TWO_PI / numberOfSquares;
+    let size = step;
+    let x = 450;
+    let y = 450;
+    
   }
